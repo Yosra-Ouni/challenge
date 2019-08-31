fc2b1b1e889f67d8228cad789a60652fdad4a79a
yosra <yosra.El.ouni@gmail.com>
2019-08-31 13:59:52 +0100

    without scss

--- src/Components/PostDetails.js
+++ src/Components/PostDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { throwStatement } from '@babel/types';
+import {Link} from 'react-router-dom'
 
 class PostDetails extends React.Component {
   state = {
@@ -12,20 +12,25 @@ class PostDetails extends React.Component {
 
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then(response => response.json())
-      .then(post => { this.setState({ post })
-    console.log(this.state.post)})
-    /*.then(() => {
-      fetch(`https://jsonplaceholder.typicode.com/users/${this.state.post.userId}`)
-        .then((user) => {
-          this.setState(() => { user })
-        }) */
+      .then(post => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+          .then(response => response.json())
+          .then((user) => {
+            this.setState({ user, post })
+          })
+        })
   }
   render() {
+    console.log(this.state)
     const showDetails = () => {
       if (this.state.user && this.state.post) {
         return (
           <div>
-            {this.state.post.id}
+            <div>{this.state.post.title}</div>
+            <div>{this.state.user.name}</div>
+            <div>{this.state.post.body}</div>
+            <br/>
+            <button><Link to={'/'}>Go back to post</Link></button>
           </div>
         )
       } else {
@@ -39,7 +44,6 @@ class PostDetails extends React.Component {
     return (
       <div>
         {showDetails()}
-        {/*{this.state.user.name} */}
       </div>
     )
   }
